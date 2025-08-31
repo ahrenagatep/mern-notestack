62555be248ad81bd3499aa40036d4c13a4db3694
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-08-30 21:53:10 -0400

    Fixed Navbar import path type in HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar.jsx";
 import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
 import api from "./lib/axios";
