8d10bf0d110a97ed908e4d1002a35f9bbdd9ed79
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-08-30 21:44:57 -0400

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -12,8 +13,17 @@ console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // middleware
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(cors({
     origin:"http://localhost:5173",
 })
@@ -30,12 +40,19 @@ app.use(rateLimiter);
 
 // endpoints (url + http to allow the client to interact with a specific resource)
 // prefixed endpoints with /api/notes
+
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT:", PORT);
     });
 })
-
-
