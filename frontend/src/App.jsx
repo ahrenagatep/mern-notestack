f2e0ca53ccccec070b02ea737c2d6dd7a2949021
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-09-17 01:29:58 -0400

    Changed HomePage background.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,26 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
+import Pattern from "./components/Pattern";
+
 const App = () => {
   return (
     <div className="relative min-h-screen w-full">
       {/* <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#3F3F3F_100%)]" /> */}
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#1a1a1a_1px)] bg-[size:20px_20px]"></div>
+      {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#1a1a1a_1px)] bg-[size:20px_20px]"></div> */}
+
+      <div className="fixed inset-0 -z-10">
+        {/* gradient background */}
+        <div className="h-full w-full bg-gradient-to-br from-gray-900 via-black to-gray-800" />
+
+        {/* dot grid background */}
+        {/* <div className="h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#1a1a1a_1px)] bg-[size:20px_20px]" /> */}
+
+        {/* <Pattern /> */}
+      </div>
+
+
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
