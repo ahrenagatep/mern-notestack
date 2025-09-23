157759be36fd1d33102039e3f49426173b41318f
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-09-22 21:17:22 -0400

    Fixed links to Insta, GitHub, LinkedIn.

--- frontend/src/components/BottomBar.jsx
+++ frontend/src/components/BottomBar.jsx
@@ -11,28 +11,25 @@ import { FaGithub, FaInstagram, FaLinkedinIn, FaMailBulk } from "react-icons/fa"
 
 const navbar = () => {
   
-  return <footer className="bg-transparent border-b border-base-content/10">
+  return <footer className="bg-transparent border-base-content/10">
     <div className="mx-auto max-w-6xl p-4">
       <div className="flex items-center justify-between">
-        <h1 className="text-lg text-primary font-light card-actions justify-center items-center">
+        <h1 className="text-xl text-primary card-actions justify-center items-center">
             NoteStack - created by Ahren Agatep
         </h1>
         <div className="flex items-center gap-4">
 
-            <Link to={"https://www.instagram.com/ahrenagatep/"}className="btn btn-ghost">
-              <FaInstagram className="size-6" />
-              {/* <span>Instagram</span> */}
-            </Link>
+            <a href="https://www.instagram.com/ahrenagatep/" target="_blank" rel="noopener noreferrer" className="btn btn-ghost">
+                <FaInstagram className="size-6" />
+            </a>
 
-            <Link to={"https://www.instagram.com/ahrenagatep/"} className="btn btn-ghost">
-              <FaGithub className="size-6" />
-              {/* <span>Github</span> */}
-            </Link>
+            <a href="https://github.com/ahrenagatep" target="_blank" rel="noopener noreferrer" className="btn btn-ghost">
+                <FaGithub className="size-6" />
+            </a>
 
-            <Link to={"https://www.linkedin.com/in/ahrenagatep/"} className="btn btn-ghost">
-              <FaLinkedinIn className="size-6" />
-              {/* <span>Connect with Me</span> */}
-            </Link>
+            <a href="https://www.linkedin.com/in/ahrenagatep/" target="_blank" rel="noopener noreferrer" className="btn btn-ghost">
+                <FaLinkedinIn className="size-6" />
+            </a>
             
         </div>
       </div>
